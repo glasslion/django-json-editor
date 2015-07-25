c469753cee0f2a409b54c6cad278314c744dfa46
Leo Zhou <glasslion@gmail.com>
2015-07-25 22:14:27 +0800

    Provide initial data in the demo form.

--- demo/demo/views.py
+++ demo/demo/views.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+import json
 
 from django.shortcuts import render
 from .forms import ContactForm
 
 def demo_home(request):
-    form = ContactForm()
+    location_example = {
+        "address" : ["126 york street", "second and third floor"],
+        "zip" : "k1n5t5",
+        "city" : "ottawa",
+        "province" : "ontario",
+        "country" : "CA",
+    }
+    form = ContactForm(initial={
+        'location':json.dumps(location_example)
+    })
     context = {}
     context['form'] = form
     return render(request, 'home.html', context)
