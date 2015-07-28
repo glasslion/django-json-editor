2e36b3cd4b3c294ae2f3f8c59ea7d11729df84e2
Leo Zhou <glasslion@gmail.com>
2015-07-28 19:02:00 +0800

    Validate form

--- demo/demo/forms.py
+++ demo/demo/forms.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from django.forms import ModelForm
+import json
 
+from django.forms import ModelForm, ValidationError
 from djjsoneditor.widgets import JSONEditorWidget
 
 from .models import Contact
@@ -13,3 +14,6 @@ class ContactForm(ModelForm):
             'location': JSONEditorWidget()
         }
 
+    def clean(self):
+        cleaned_data = super(ContactForm, self).clean()
+        location = cleaned_data.get("location")
